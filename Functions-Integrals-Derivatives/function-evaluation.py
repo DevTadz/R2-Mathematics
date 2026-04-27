157dbf774691397c9e00ba65df5394f31cfa82cd
TadzMain <joaworkmail@gmail.com>
2026-04-27 20:37:46 +0200

    Finnished function eval

--- Functions-Integrals-Derivatives/function-evaluation.py
+++ Functions-Integrals-Derivatives/function-evaluation.py
@@ -1,1 +1,8 @@
-#Placeholder
+def f(x):
+    return 3*(x**2)-(4*x)+1
+
+evaluation = [-2, -1, 0, 1, 2]
+
+for i in range(len(evaluation)):
+    x = evaluation[i]
+    print(f"x: {x}", f"| f(x): {f(x)}")
